4ce2e74f192d21aae83af5553decb1c198e1d93d
sridharyalla <Sridhar2007@gmail.com>
2021-01-29 18:44:35 +0530

    Update main.go

--- 70_Climbing_Stairs/main.go
+++ 70_Climbing_Stairs/main.go
@@ -1,34 +1,30 @@
 package main
 
-func max(a int, b int) int {
+func Max(a int, b int) int {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func climbStairs1(n int) int {
-	dp := make([]int, max(3, n+1))
-	dp[0] = 1
-	dp[1] = 1
-	dp[2] = 2
-
-	for i := 3; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+func test1 () int{
+	if true {
+		return 0
+	} else {
+		return 1
 	}
-
-	return dp[n]
-}
 
 func climbStairs(n int) int {
-	dp := make([]int, max(3, n+1))
+	dp := make([]int, Max(3, n+1))
 	dp[0] = 1
-	dp[1] = 1
+		dp[1] = 1
 	dp[2] = 2
 
 	for i := 3; i <= n; i++ {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
-
+	
+	test1()
+	
 	return dp[n]
 }
