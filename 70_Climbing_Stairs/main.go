24818816eb323c832aa2dc5f5cbfdc0a3584f8f8
sridharyalla <Sridhar2007@gmail.com>
2021-01-29 18:31:20 +0530

    Update main.go

--- 70_Climbing_Stairs/main.go
+++ 70_Climbing_Stairs/main.go
@@ -19,3 +19,16 @@ func climbStairs(n int) int {
 
 	return dp[n]
 }
+
+func climbStairs(n int) int {
+	dp := make([]int, max(3, n+1))
+	dp[0] = 1
+	dp[1] = 1
+	dp[2] = 2
+
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
